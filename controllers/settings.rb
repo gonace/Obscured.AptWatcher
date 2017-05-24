bfbebd80b874029ab1735fd443c95fc0d690aeed
Erik Hennerfors <erik.hennerfors@adeprimo.se>
2017-05-24 09:03:44 +0200

    Fixed a few bugs in the Settings controller

--- controllers/settings.rb
+++ controllers/settings.rb
@@ -8,8 +8,34 @@ module Obscured
         get '/' do
           authorize!
 
-          heroku = PlatformAPI.connect_oauth(ENV['HEROKU_TOKEN'])
-          heroku_vars = heroku.config_var.info_for_app('aptwatcher')
+          unless ENV['HEROKU_TOKEN'].blank?
+            heroku = PlatformAPI.connect_oauth(ENV['HEROKU_TOKEN'])
+            heroku_vars = heroku.config_var.info_for_app('aptwatcher')
+          else
+            heroku_vars = {
+              'ADMIN_EMAIL' => '',
+              'ADMIN_PASSWORD' => '',
+              'API_PASSWORD' => '',
+              'API_USERNAME' => '',
+              'HEROKU_TOKEN' => '',
+              'LANG' => '',
+              'MONGODB_URI' => '',
+              'RACK_ENV' => '',
+              'RAYGUN_KEY' => '',
+              'SENDGRID_DOMAIN' => '',
+              'SENDGRID_PASSWORD' => '',
+              'SENDGRID_PORT' => '',
+              'SENDGRID_SERVER' => '',
+              'SENDGRID_USERNAME' => '',
+              'SLACK_CHANNEL' => '',
+              'SLACK_ICON' => '',
+              'SLACK_USER' => '',
+              'SLACK_WEBHOOK_URL' => '',
+              'TZ' => '',
+              'USER_CONFIRMATION' => '',
+              'USER_REGISTRATION' => ''
+            }
+          end
 
           haml :index, :locals => { :config => heroku_vars }
         end
